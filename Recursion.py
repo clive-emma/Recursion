444f56ad2b324084dff9d836c3283884a895e6b9
clive-strath <clive.sekandi@strathmore.edu>
2025-05-05 10:00:20 +0300

    first commit

--- Recursion.py
+++ Recursion.py
@@ -24,12 +24,24 @@ from functools import lru_cache
 # for i in range(1,500):
 #     print(f"{i} Term:{fib2(i)}")
 
-@lru_cache(maxsize=1000)
-def fib3(n):
-    if n == 1 or n == 2:
-        return 1
-    elif n > 2:
-        return fib3(n - 1)+fib3(n - 2)
+# @lru_cache(maxsize=1000)
+# def fib3(n):
+#     if n == 1 or n == 2:
+#         return 1
+#     elif n > 2:
+#         return fib3(n - 1)+fib3(n - 2)
+#
+# for i in range(1,10000):
+#     print(f"{i} Term:{fib3(i)}")
 
-for i in range(1,500):
-    print(f"{i} Term:{fib3(i)}")
+
+def TowerOfHanoi(n,source,destination_rod,auxilliary_rod):
+    if n==1:
+        print("Move disk 1 from source",source,"to destination",destination_rod)
+        return
+    TowerOfHanoi(n-1,source,auxilliary_rod,destination_rod)
+    print("Move disk ",n," from source", source, "to destination", destination_rod)
+    TowerOfHanoi(n - 1, auxilliary_rod, destination_rod,source)
+
+n=3
+TowerOfHanoi(n,"A","B","C")
